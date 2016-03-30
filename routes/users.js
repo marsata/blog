2f33e3a4990ef2f82148d5ba99c9502bab09e55e
mars <marsata.mo@gmail.com>
2016-03-30 18:11:32 +0800

    commit with Webstorm

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('I\'m Mars');
 });
 
 module.exports = router;
